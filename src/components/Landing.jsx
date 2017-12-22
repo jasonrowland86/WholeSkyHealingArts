e18c00dd74748383df0c31b9aba805a31e191087
Jason Rowland <jasonrowland86>
2017-12-22 16:35:45 -0600

    Added state to landing page component to change background img

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
-const Landing = () => {
-  return (
-    <div id="background" className="landing landing-img" style = {{background: `url(../Dan-sunset.jpg) no-repeat center center fixed`, backgroundSize: 'cover'}}>
-      <div className="header">
-        <nav>
-          <Link className="landing-nav landing-logo" to="/">Logo</Link>
-          <Link className="landing-nav-hack" to=""></Link>
-          <NavLink className="landing-nav nav-link" link-attr="Services" to="/services" activeClassName="active"></NavLink>
-          <NavLink className="landing-nav nav-link" link-attr="About" to="/about" activeClassName="active"></NavLink>
-          <NavLink className="landing-nav nav-link" link-attr="Contact" to="/contact" activeClassName="active"></NavLink>
-        </nav>
+class Landing extends Component {
+  constructor() {
+    super();
+    this.state = {
+      backgroundImg: `url(../Dan-sunset.jpg) no-repeat center center fixed`,
+    }
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
+  }
+
+  handleMouseOver() {
+    this.setState({
+      backgroundImg: `url(../Dan-hands.jpg) no-repeat center center fixed`,
+    });
+  }
+
+  handleMouseOut() {
+    this.setState({
+      backgroundImg: `url(../Dan-sunset.jpg) no-repeat center center fixed`,
+    })
+  }
+
+  render() {
+    return (
+      <div className="landing landing-img" style={{background: this.state.backgroundImg, backgroundSize: 'cover'}}>
+        <div className="header">
+          <nav>
+            <Link className="landing-nav landing-logo" to="/">Logo</Link>
+            <Link className="landing-nav-hack" to=""></Link>
+            <NavLink className="landing-nav nav-link" link-attr="Services" to="/services" activeClassName="active"></NavLink>
+            <NavLink className="landing-nav nav-link" link-attr="About" to="/about" activeClassName="active"></NavLink>
+            <NavLink className="landing-nav nav-link" link-attr="Contact" to="/contact" activeClassName="active"></NavLink>
+          </nav>
+        </div>
+        <h1 onMouseEnter={this.handleMouseOver} onMouseLeave={this.handleMouseOut} className='landing-title'>WHOLE SKY HEALING ARTS</h1>
       </div>
-      <h1 id="title" className='landing-title'>WHOLE SKY HEALING ARTS</h1>
-    </div>
-  )
+    )
+  }
 }
 
 

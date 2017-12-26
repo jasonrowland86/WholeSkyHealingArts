bcd90fc1d29c72ddd858ee4b1c8c72c0a25cbd95
Jason Rowland <jasonrowland86>
2017-12-26 15:57:14 -0600

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import FontAwesome from 'react-fontawesome';
 
 class Header extends Component {
   constructor() {
     super();
     this.state = {
-      didScroll: false,
+
     }
 
   }
@@ -18,11 +19,15 @@ class Header extends Component {
     return (
       <div className="header">
         <nav className="nav-w-color">
-          <Link className="logo" to="/" style={{ color: 'black' }}>Logo</Link>
+          <Link className="logo" to="/" style={{ color: 'black' }}><img className="about-img" src="photos/Hands_Circle.png" style={{ width: '30px' }}></img></Link>
           <Link className="landing-nav-hack" to=""></Link>
           <NavLink className="nav-link" link-attr="Services" to="/services" activeClassName="active" ></NavLink>
           <NavLink className="nav-link" link-attr="About" to="/about" activeClassName="active" ></NavLink>
           <NavLink className="nav-link" link-attr="Contact" to="/contact" activeClassName="active" ></NavLink>
+
+          <a href="#" target="blank"><FontAwesome className="icon" name="facebook" size="1x"/></a>
+          <a href="#" target="blank"><FontAwesome className="icon" name="instagram" size="1x"/></a>
+
         </nav>
       </div>
     )

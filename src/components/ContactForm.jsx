97180b04331aa37c2c9dbec3a2ef086db0357ad3
Jason Rowland <jasonrowland86>
2017-12-26 15:58:59 -0600

    Reformatted spacing in contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,22 +4,22 @@ const ContactForm = () => {
   return(
     <div className="contact-form">
 
-      <h1 id="connect">Contact me for availability:</h1>
+      <h3 id="connect">Contact for booking</h3>
 
       <form id="gform" action="https://script.google.com/macros/s/AKfycbxl5LpPm_0ch0P3BzKRapFNbFlUVyPHsIfIZVUjfeEr1qTWzGFz/exec" method="POST">
         <label for="name">Name:</label><br></br>
         <input id="name" type="text" name="name" size="30"></input><br></br>
-
+        <br></br>
         <label for="email">Your Email:</label><br></br>
         <input type="text" name="mail" size="30"></input><br></br>
         <span id="email-invalid">Must be a valid email address</span>
-
+        <br></br>
         <label for="message">Message:</label><br></br>
         <textarea type="text" name="comment"></textarea><br></br>
 
         <input className="submit" type="submit" value="Send"></input>
       </form>
-
+      <br></br>
     </div>
   )
 }

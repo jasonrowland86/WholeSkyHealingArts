c8ce2d881e4a8ede7009347428e8a28cba833e74
Jason Rowland <jasonrowland86>
2017-12-26 15:56:48 -0600

    reformatted content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,15 +8,24 @@ const Contact = () => {
     <div className="contact">
       <Header />
 
+      <h3 className="contact-heading">Located in Greenpoint Brooklyn</h3>
+
       <div className="map">
         <MyMap />
       </div>
 
       <div className="contact-info">
-        <h3>Located in Greenpoint Brooklyn</h3>
+
+        <h3>Available</h3>
         <p>Tuesday - Saturday</p>
+
+        <div className="media-links">
+
+        </div>
+
       <ContactForm />
       </div>
+
     </div>
   )
 }

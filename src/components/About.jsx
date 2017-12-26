070ff17cf09094235bf867b363f9fe0104464760
Jason Rowland <jasonrowland86>
2017-12-26 15:56:36 -0600

    Removed media links, reformatted content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
 import Header from './Header';
-import FontAwesome from 'react-fontawesome';
 
 const About = () => {
   return (
     <div className="main">
       <Header />
-      <div className="main-content">
+      <div className="about main-content">
         <div className="about-left left-side">
           <img className="about-img" src="photos/Dan-full.jpg"></img>
         </div>
         <div className="right-side">
 
           <h3>Whole Sky Healing Arts</h3>
-          <p>Is dedicated to the expansion of human life-force energy.</p>
+          <p>Dedicated to the expansion of human life-force.</p>
           <p>Our energy flow affects how we feel, how we think, and the overall state of our well being.</p>
-          <p>Energy blockages can be the result of injuries, traumas, diets, addictions,
+          <p>Energy blocks can be the result of injuries, traumas, diets, addictions,
             lack of excercise, and most of all stress.</p>
           <p>Our goal is to restore proper
             balance, alignment, vibration and energy flow in the human body. This will
             be achieved through the combination and application of various bodywork
             and energy balancing methods.</p>
 
-          <div className="about-right">
-            <h3>Find me on:</h3>
-            <a className="social-media-link"><p>Instagram</p></a>
-            <a className="social-media-link"><p>Facebook</p></a>
-
-            <a href="#" target="blank"><FontAwesome className="icon" name="facebook" size="1x"/></a>
-            <a href="#" target="blank"><FontAwesome className="icon" name="instagram" size="1x"/></a>
+          <div className="about-mobile">
+            <h3>
+              Daniel Duron is a certified IPHM massage and bodywork (Alchemy of Touch)
+              practitioner, and Universal life-force energy (Reiki) provider.
+            </h3>
+            <p>
+              Alchemy of Touch is a fusion of Rebalancing massage from India that
+              integrates Deep Tissue massage, Myofascial release method, correct breathing,
+              meditative healing approach, visceral work, polarity energy work and
+              Thai yoga stretches. This method was developed by Tapesh Paradiso and
+              expanded with the expertise of Anouk Hornman.
+            </p>
           </div>
 
         </div>
